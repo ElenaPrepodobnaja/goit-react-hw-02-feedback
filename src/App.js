55bc25c8ff8e865c83f05aec628f5ee5a9d8fc89
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-07-14 23:07:27 +0300

    HW-2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Feedback from './components/Feedback';
+
+class App extends Component {
+
+
+  render() {
+
+    return <Feedback initialValue={0} />;
+  }
+}
+
+export default App;
